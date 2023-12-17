2d3c5cf953b0fc021779d4e48c381cfd413511d5
Jonas Langlotz <me@jonas-langlotz.de>
2023-12-17 01:39:30 +0100

    Add WeatherData provider to DataModel

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config'
-import { cleanEnv, num } from 'envalid'
+import { cleanEnv, num, str } from 'envalid'
 
 export const env = cleanEnv(process.env, {
-  WEBSOCKET_SERVER_PORT: num()
+  WEBSOCKET_SERVER_PORT: num(),
+  PIRATE_WEATHER_API_KEY: str(),
+  LATITUDE: num(),
+  LONGITUDE: num(),
+  WEATHER_DATA_FETCH_INTERVAL: num()
 })
