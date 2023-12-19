221ce7e0748ec3bc688b55925db6bcdeef451b14
Jonas Langlotz <me@jonas-langlotz.de>
2023-12-19 20:38:37 +0100

    Improve IcalEventProvider and tests

--- server/src/data/data-providers/ical.ts
+++ server/src/data/data-providers/ical.ts
@@ -9,8 +9,32 @@ interface Options {
 }
 
 function shiftDateByDays (date: Date, days: number): Date {
-  date.setDate(date.getDate() + days)
-  return date
+  const shiftedDate = new Date(date)
+  shiftedDate.setDate(shiftedDate.getDate() + days)
+  return shiftedDate
+}
+
+function dateAsUTC (date: Date): Date {
+  const result = new Date(date)
+  result.setMinutes(result.getMinutes() - result.getTimezoneOffset())
+  return result
+}
+
+function daysBetween (startDate: Date, endDate: Date): number {
+  const millisecondsPerDay = 24 * 60 * 60 * 1000
+  return (dateAsUTC(endDate).valueOf() - dateAsUTC(startDate).valueOf()) / millisecondsPerDay
+}
+
+function toISODateString (date: Date): string {
+  return dateAsUTC(date).toISOString().split('T')[0]
+}
+
+function uniqBy<T, K extends PropertyKey> (array: T[], keyFunction: (item: T) => K): T[] {
+  const seen: Partial<Record<K, boolean>> = {}
+  return array.filter(function (item) {
+    const key = keyFunction(item)
+    return Object.prototype.hasOwnProperty.call(seen, key) ? false : (seen[key] = true)
+  })
 }
 
 export class IcalEventsProvider extends DataProvider<Events> {
@@ -52,26 +76,46 @@ export class IcalEventsProvider extends DataProvider<Events> {
   }
 
   _filterAndConvertRawEvent (rawEvent: Ical.VEvent): Event[] {
-    const startOfTimeSpan = shiftDateByDays(new Date(), -this.eventTimeSpanDays)
-    const endOfTimeSpan = shiftDateByDays(new Date(), this.eventTimeSpanDays)
+    const relevanceStart = shiftDateByDays(new Date(), -this.eventTimeSpanDays)
+    const relevanceEnd = shiftDateByDays(new Date(), this.eventTimeSpanDays)
 
-    if (rawEvent.rrule == null) {
-      if (rawEvent.start < startOfTimeSpan || rawEvent.end > endOfTimeSpan) {
-        return []
+    const dates: Date[] = []
+
+    if (this._rawEventHasTime(rawEvent)) {
+      if (rawEvent.rrule != null) {
+        dates.push(...rawEvent.rrule.between(relevanceStart, relevanceEnd))
+      } else if (rawEvent.start > relevanceStart) {
+        dates.push(rawEvent.start)
+      }
+    } else {
+      const eventLengthInDays = Math.ceil(daysBetween(rawEvent.start, rawEvent.end))
+
+      const startingDates: Date[] = []
+      // A multi-day event may start before the relevance period starts,
+      // but could still intersect it, so we shift the relevance dates
+      if (rawEvent.rrule != null) {
+        startingDates.push(...rawEvent.rrule.between(shiftDateByDays(relevanceStart, -eventLengthInDays), relevanceEnd))
+      } else if (rawEvent.start > shiftDateByDays(relevanceStart, -eventLengthInDays)) {
+        startingDates.push(rawEvent.start)
       }
 
-      return [{
-        name: rawEvent.summary,
-        date: rawEvent.start.toISOString(),
-        hasTime: this._rawEventHasTime(rawEvent)
-      }]
-    }
+      const allDates = startingDates
+        .flatMap(startingDate => {
+          const endingDate = shiftDateByDays(startingDate, eventLengthInDays)
 
-    const dates = rawEvent.rrule.between(startOfTimeSpan, endOfTimeSpan)
+          const firstRelevantDate = startingDate < relevanceStart ? relevanceStart : startingDate
+          const lastRelevantDate = endingDate > relevanceEnd ? relevanceEnd : endingDate
+          const relevantDayCount = daysBetween(firstRelevantDate, lastRelevantDate)
+
+          return Array.from({ length: relevantDayCount }, (_, offset) => shiftDateByDays(firstRelevantDate, offset))
+        })
+
+      dates.push(...uniqBy(allDates, date => toISODateString(date)))
+    }
 
     return dates.map(date => ({
       name: rawEvent.summary,
-      date: date.toISOString(),
+      date: this._rawEventHasTime(rawEvent) ? date.toISOString() : toISODateString(date),
       hasTime: this._rawEventHasTime(rawEvent)
     }))
   }
--- server/test/unit/data/data-providers/ical.test.ts
+++ server/test/unit/data/data-providers/ical.test.ts
@@ -1,14 +1,14 @@
 import chai, { assert } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import sinon, { type SinonFakeTimers, type SinonStub } from 'sinon'
-import { type Events } from '../../../../../shared-types'
 import { IcalEventsProvider } from '../../../../src/data/data-providers/ical'
 import { DashboardError } from '../../../../src/utils/dashboard-error'
 
 chai.use(chaiAsPromised)
 
 describe('IcalEventsProvider', function () {
-  const icalData = `BEGIN:VCALENDAR
+  function inCalendar (data: string): string {
+    return `BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//ical.marudot.com//iCal Event Maker
 CALSCALE:GREGORIAN
@@ -32,74 +32,13 @@ DTSTART:19701025T030000
 RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU
 END:STANDARD
 END:VTIMEZONE
-BEGIN:VEVENT
-DTSTAMP:20231218T010638Z
-UID:1702861500416-13327@ical.marudot.com
-DTSTART;VALUE=DATE:20231224
-DTEND;VALUE=DATE:20231225
-SUMMARY:One-Time All-Day Event
-END:VEVENT
-BEGIN:VEVENT
-DTSTAMP:20231218T010638Z
-UID:1702861570327-65854@ical.marudot.com
-DTSTART;VALUE=DATE:20231224T
-RRULE:FREQ=DAILY;INTERVAL=2
-DTEND;VALUE=DATE:20231225
-SUMMARY:Recurring All-Day Event
-END:VEVENT
-BEGIN:VEVENT
-DTSTAMP:20231218T010638Z
-UID:1702861466053-54879@ical.marudot.com
-DTSTART;TZID=Europe/Berlin:20231224T120000
-DTEND;TZID=Europe/Berlin:20231224T130000
-SUMMARY:One-Time Event
-END:VEVENT
-BEGIN:VEVENT
-DTSTAMP:20231218T010638Z
-UID:1702861545999-91612@ical.marudot.com
-DTSTART;TZID=Europe/Berlin:20231224T120000
-RRULE:FREQ=DAILY;INTERVAL=2
-DTEND;TZID=Europe/Berlin:20231224T130000
-SUMMARY:Recurring Event
-END:VEVENT
+${data}
 END:VCALENDAR`
+  }
 
-  const events: Events = [
-    {
-      name: 'One-Time All-Day Event',
-      date: '2023-12-23T23:00:00.000Z',
-      hasTime: false
-    },
-    {
-      name: 'One-Time Event',
-      date: '2023-12-24T11:00:00.000Z',
-      hasTime: true
-    },
-    {
-      name: 'Recurring Event',
-      date: '2023-12-24T11:00:00.000Z',
-      hasTime: true
-    },
-    {
-      name: 'Recurring Event',
-      date: '2023-12-26T11:00:00.000Z',
-      hasTime: true
-    },
-    {
-      name: 'Recurring All-Day Event',
-      date: '2023-12-24T00:00:00.000Z',
-      hasTime: false
-    },
-    {
-      name: 'Recurring All-Day Event',
-      date: '2023-12-26T00:00:00.000Z',
-      hasTime: false
-    }
-  ]
-
-  async function positiveResponse (): Promise<Response> {
+  async function positiveResponse (data: string): Promise<Response> {
     return new Response(
-      icalData,
+      data,
       { status: 200, headers: { 'content-type': 'text/plain' } }
     )
   }
@@ -131,13 +70,188 @@ END:VCALENDAR`
   })
 
   describe('#getData', function () {
-    it('should return the correct Events when fetch returns valid data', async function () {
-      fetchStub.onFirstCall().returns(positiveResponse())
+    it('should correctly parse multiple events', async function () {
+      const icalData = inCalendar(`BEGIN:VEVENT
+DTSTAMP:20231218T010638Z
+UID:1702861466053-54879@ical.marudot.com
+DTSTART;TZID=Europe/Berlin:20231224T120000
+DTEND;TZID=Europe/Berlin:20231224T130000
+SUMMARY:One-Time Event
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20231218T010639Z
+UID:1702861466054-54879@ical.marudot.com
+DTSTART;TZID=Europe/Berlin:20231225T120000
+DTEND;TZID=Europe/Berlin:20231225T130000
+SUMMARY:One-Time Event 2
+END:VEVENT`)
+
+      fetchStub.onFirstCall().returns(positiveResponse(icalData))
 
       const value = await subject.getData()
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      assert.sameDeepMembers(value, events)
+      assert.deepEqual(value, [{
+        name: 'One-Time Event',
+        date: '2023-12-24T11:00:00.000Z',
+        hasTime: true
+      }, {
+        name: 'One-Time Event 2',
+        date: '2023-12-25T11:00:00.000Z',
+        hasTime: true
+      }])
+    })
+
+    it('should correctly parse a one-time event', async function () {
+      const icalData = inCalendar(`BEGIN:VEVENT
+DTSTAMP:20231218T010638Z
+UID:1702861466053-54879@ical.marudot.com
+DTSTART;TZID=Europe/Berlin:20231224T120000
+DTEND;TZID=Europe/Berlin:20231224T130000
+SUMMARY:One-Time Event
+END:VEVENT`)
+
+      fetchStub.onFirstCall().returns(positiveResponse(icalData))
+
+      const value = await subject.getData()
+
+      assert.deepEqual(value, [{
+        name: 'One-Time Event',
+        date: '2023-12-24T11:00:00.000Z',
+        hasTime: true
+      }])
+    })
+
+    it('should correctly parse a one-time all-day event', async function () {
+      const icalData = inCalendar(`BEGIN:VEVENT
+DTSTAMP:20231218T010638Z
+UID:1702861500416-13327@ical.marudot.com
+DTSTART;VALUE=DATE:20231224
+DTEND;VALUE=DATE:20231225
+SUMMARY:One-Time All-Day Event
+END:VEVENT`)
+
+      fetchStub.onFirstCall().returns(positiveResponse(icalData))
+
+      const value = await subject.getData()
+
+      assert.deepEqual(value, [{
+        name: 'One-Time All-Day Event',
+        date: '2023-12-24',
+        hasTime: false
+      }])
+    })
+
+    it('should correctly parse a recurring event', async function () {
+      const icalData = inCalendar(`BEGIN:VEVENT
+DTSTAMP:20231218T010638Z
+UID:1702861545999-91612@ical.marudot.com
+DTSTART;TZID=Europe/Berlin:20231224T120000
+RRULE:FREQ=DAILY;INTERVAL=2
+DTEND;TZID=Europe/Berlin:20231224T130000
+SUMMARY:Recurring Event
+END:VEVENT`)
+
+      fetchStub.onFirstCall().returns(positiveResponse(icalData))
+
+      const value = await subject.getData()
+
+      assert.deepEqual(value, [{
+        name: 'Recurring Event',
+        date: '2023-12-24T11:00:00.000Z',
+        hasTime: true
+      },
+      {
+        name: 'Recurring Event',
+        date: '2023-12-26T11:00:00.000Z',
+        hasTime: true
+      }])
+    })
+
+    it('should correctly parse a recurring all-day event', async function () {
+      const icalData = inCalendar(`BEGIN:VEVENT
+DTSTAMP:20231218T010638Z
+UID:1702861570327-65854@ical.marudot.com
+DTSTART;VALUE=DATE:20231224T
+RRULE:FREQ=DAILY;INTERVAL=2
+DTEND;VALUE=DATE:20231225
+SUMMARY:Recurring All-Day Event
+END:VEVENT`)
+
+      fetchStub.onFirstCall().returns(positiveResponse(icalData))
+
+      const value = await subject.getData()
+
+      assert.deepEqual(value, [{
+        name: 'Recurring All-Day Event',
+        date: '2023-12-24',
+        hasTime: false
+      },
+      {
+        name: 'Recurring All-Day Event',
+        date: '2023-12-26',
+        hasTime: false
+      }])
+    })
+
+    it('should correctly parse a one-time multi-day event', async function () {
+      const icalData = inCalendar(`BEGIN:VEVENT
+DTSTAMP:20231218T173040Z
+UID:1702920613045-27816@ical.marudot.com
+DTSTART;VALUE=DATE:20231223
+DTEND;VALUE=DATE:20231226
+SUMMARY:One-Time Multi-Day Event
+END:VEVENT`)
+
+      fetchStub.onFirstCall().returns(positiveResponse(icalData))
+
+      const value = await subject.getData()
+
+      assert.deepEqual(value, [23, 24, 25].map(day => ({
+        name: 'One-Time Multi-Day Event',
+        date: `2023-12-${day}`,
+        hasTime: false
+      })))
+    })
+
+    it('should correctly parse a multi-day event starting outside of the configured time span', async function () {
+      const icalData = inCalendar(`BEGIN:VEVENT
+DTSTAMP:20231219T184103Z
+UID:1702920613045-27816@ical.marudot.com
+DTSTART;VALUE=DATE:20231201
+DTEND;VALUE=DATE:20231231
+SUMMARY:Outside-Interval One-Time Multi-Day Event
+END:VEVENT`)
+
+      fetchStub.onFirstCall().returns(positiveResponse(icalData))
+
+      const value = await subject.getData()
+
+      assert.deepEqual(value, [21, 22, 23, 24, 25, 26].map(day => ({
+        name: 'Outside-Interval One-Time Multi-Day Event',
+        date: `2023-12-${day}`,
+        hasTime: false
+      })))
+    })
+
+    it('should correctly parse a recurring multi-day event', async function () {
+      const icalData = inCalendar(`BEGIN:VEVENT
+DTSTAMP:20231219T184103Z
+UID:1703011251979-11629@ical.marudot.com
+DTSTART;VALUE=DATE:20231222
+RRULE:FREQ=DAILY;INTERVAL=3
+DTEND;VALUE=DATE:20231224
+SUMMARY:Recurring Multi-Day Event
+END:VEVENT`)
+
+      fetchStub.onFirstCall().returns(positiveResponse(icalData))
+
+      const value = await subject.getData()
+
+      assert.deepEqual(value, [22, 23, 25, 26].map(day => ({
+        name: 'Recurring Multi-Day Event',
+        date: `2023-12-${day}`,
+        hasTime: false
+      })))
     })
   })
 
